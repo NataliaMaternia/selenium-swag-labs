2c1a1159a623b746167b6ed6c4a3315148491581
Natalia Maternia <nat.maternia@gmail.com>
2024-02-08 15:57:47 +0100

    Check if all product pages are accessible

--- src/test/java/MainPage.java
+++ src/test/java/MainPage.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
-import java.util.ListIterator;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,24 +24,25 @@ public class MainPage {
 
     public void clickOnProductsAndBack() throws InterruptedException {
         List<WebElement> productList = driver.findElements(By.cssSelector(".inventory_item_label a"));
-        for (int x = 1; x <= productList.size(); x++){
-            WebElement productLink = driver.findElement(By.cssSelector("div.inventory_item:nth-child(" + x + ") a"));
+        for (int x = 1; x <= productList.size(); x++) {
+            WebElement productLink = getProductLinkByPositionInList(x);
             productLink.click();
             TimeUnit.SECONDS.sleep(1);
             WebElement backButton = driver.findElement(By.id("back-to-products"));
             backButton.click();
             TimeUnit.SECONDS.sleep(1);
-
         }
-
-
-        }
-
-
-
-
-
     }
+
+    private WebElement getProductLinkByPositionInList(int positionOfProduct) {
+        String selectorForProductLinkByPosition =
+                "div.inventory_item:nth-child(" + positionOfProduct + ") a";
+        WebElement productLink = driver.findElement(By.cssSelector(selectorForProductLinkByPosition));
+        return productLink;
+    }
+
+
+}
 
 
 
